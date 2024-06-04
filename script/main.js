196a529eb0673d57a5b85ec53ef6efe3bd51b018
babucarr32 <baboucarry@gmai.com>
2024-06-04 07:37:47 +0000

    delete icons folder

--- script/main.js
+++ script/main.js
@@ -27,46 +27,57 @@ const git = simpleGit();
 try {
   const repos = [];
 
-  for (let i = 0; i < icons.length; i += 1) {
-    const source = icons[i].source.url;
-    const localName = icons[i].source.localName;
+  // for (let i = 0; i < icons.length; i += 1) {
+  //   const source = icons[i].source.url;
+  //   const localName = icons[i].source.localName;
 
-    /*
-    First pull project, if it does not exist, then clone.
-     */
-    try {
-      const repoName = await handleGitPull(localName);
-      if (repoName) {
-        repos.push(repoName);
-      }
-    } catch (error) {
-      if (
-        error.message ===
-        "Cannot use simple-git on a directory that does not exist"
-      ) {
-        repos.push(localName);
+  //   /*
+  //   First pull project, if it does not exist, then clone.
+  //    */
+  //   try {
+  //     const repoName = await handleGitPull(localName);
+  //     if (repoName) {
+  //       repos.push(repoName);
+  //     }
+  //   } catch (error) {
+  //     if (
+  //       error.message ===
+  //       "Cannot use simple-git on a directory that does not exist"
+  //     ) {
+  //       repos.push(localName);
 
-        try {
-          console.log(
-            "Cloning",
-            source,
-            "to",
-            `${path.join(process.cwd(), "icons", localName)}`
-          );
-          await git.clone(
-            source,
-            path.join(process.cwd(), path.join("icons", localName))
-          );
-        } catch (error) {
-          console.log(error);
-        }
-      }
+  //       try {
+  //         console.log(
+  //           "Cloning",
+  //           source,
+  //           "to",
+  //           `${path.join(process.cwd(), "icons", localName)}`
+  //         );
+  //         await git.clone(
+  //           source,
+  //           path.join(process.cwd(), path.join("icons", localName))
+  //         );
+  //       } catch (error) {
+  //         console.log(error);
+  //       }
+  //     }
+  //   }
+  // }
+  // console.log("Git task done... \n");
+
+  // await convertSVGToMDX(repos);
+  // await generateContent(repos);
+  await fsPromise.rm(
+    path.join(
+      process.cwd(),
+      `icons/`
+    ),
+    {
+      recursive: true,
+      force: true,
     }
-  }
-  console.log("Git task done... \n");
-
-  await convertSVGToMDX(repos);
-  await generateContent(repos);
+  );
+  console.log("Delete success...")
 } catch (error) {
   console.error(error.message);
 }
